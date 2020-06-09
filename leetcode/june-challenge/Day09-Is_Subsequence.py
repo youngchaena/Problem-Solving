5f96aab796c11736a4b771694e569137578bc83c
Youngchae Na <youngchaena@naver.com>
2020-06-09 22:00:49 +0900

    solve leetcode challenge Day 09

---
+++ leetcode/june-challenge/Day09-Is_Subsequence.py
@@ -0,0 +1,14 @@
+# Day 09 - Is Subsequence
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0: return True
+        if len(t) == 0: return False
+        
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        
+        return True if i == len(s) else False
