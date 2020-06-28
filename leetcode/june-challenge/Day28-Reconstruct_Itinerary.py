44d7318ca67599f1697794bea2c89349024f7e52
Youngchae Na <youngchaena@naver.com>
2020-06-28 21:57:06 +0900

    solve leetcode challenge Day 28

---
+++ leetcode/june-challenge/Day28-Reconstruct_Itinerary.py
@@ -0,0 +1,16 @@
+# Day 28 - Reconstruct Itinerary
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def dfs(city):
+            while(len(graph[city]) > 0):
+                dfs(graph[city].pop(0))
+            res.insert(0, city)
+        
+        graph = collections.defaultdict(list)
+        for u, v in tickets:
+            graph[u].append(v)
+            graph[u].sort()
+        res = []
+        dfs('JFK')
+        return res
