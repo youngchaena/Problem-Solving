d8d43726447b094f834a848d7cf580f0131f3cdb
Youngchae Na <youngchaena@naver.com>
2020-08-02 16:35:14 +0900

    solve some string questions

---
+++ leetcode/125_Valid_Palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+        
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+            
+        return True
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+        
+        return s == s[::-1]
